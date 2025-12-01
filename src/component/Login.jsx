0c8c50bdd931824a91dc3b302771fb2d8d2cb793
rozitasm <roz.sama99@gmail.com>
2025-12-01 18:07:41 +0330

    Update

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -71,7 +71,8 @@ function Login() {
           <div className="flex flex-col gap-4 w-full max-w-xs">
             <input
               type="text"
-              placeholder="Username"
+              placeholder="Username "
+              value={Admin}
               onChange={userhandler}
               className="p-3 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-[#5AC1F0]"
             />
